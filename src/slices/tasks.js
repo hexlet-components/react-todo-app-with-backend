d53af81e63307799bf09865c60f98076fa19bc02
ysemenyuk <invent@list.ru>
2021-04-08 00:40:51 +0300

    add adapter for lists and tasks

--- src/slices/tasks.js
+++ src/slices/tasks.js
@@ -1,95 +1,92 @@
-// @ts-nocheck
-
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 import routes from '../routes.js';
+import adapter from './adapter.js';
 
-export const addTaskThunk = createAsyncThunk(
+export const fetchAllTasks = createAsyncThunk(
+  'tasks/fetchAllTasks',
+  async () => {
+    const url = routes.tasks();
+    const response = await axios.get(url);
+    console.log('fetchAllTasksThunk response.data -', response.data);
+    return response.data.tasks;
+  }
+);
+
+export const fetchTasksByListId = createAsyncThunk(
+  'tasks/fetchTasksByListId',
+  async ({ currentListId }) => {
+    const url = routes.listTasks(currentListId);
+    const response = await axios.get(url);
+    console.log('fetchTasksByListIdThunk response.data -', response.data);
+    return response.data.tasks;
+  }
+);
+
+// export const fetchTasks = createAsyncThunk(
+//   'tasks/fetchTasks',
+//   async ({ currentListId }) => {
+//     const url = currentListId
+//       ? routes.listTasks(currentListId)
+//       : routes.tasks();
+//     const response = await axios.get(url);
+//     console.log('fetchTasksThunk response.data -', response.data);
+//     return response.data.tasks;
+//   }
+// );
+
+export const addTask = createAsyncThunk(
   'tasks/addTask',
   async ({ text, listId }) => {
     const url = routes.tasks();
     const response = await axios.post(url, { text, listId });
     console.log('addTaskThunk response.data -', response.data);
-    return response.data;
+    return response.data.task;
   }
 );
 
-export const removeTaskThunk = createAsyncThunk(
+export const updateTask = createAsyncThunk(
+  'tasks/updateTask',
+  async ({ id, completed }) => {
+    const url = routes.task(id);
+    const response = await axios.patch(url, { completed });
+    console.log('updateTaskThunk response -', response);
+    return response.data.task;
+  }
+);
+
+export const removeTask = createAsyncThunk(
   'tasks/removeTask',
   async ({ id }) => {
     const url = routes.task(id);
     const response = await axios.delete(url);
-    console.log('removeTaskThunk response -', response);
+    console.log('deleteTaskThunk response -', response);
     return id;
-  }
-);
-
-export const toggleTaskStateThunk = createAsyncThunk(
-  'tasks/toggleTaskState',
-  async ({ id, completed }) => {
-    const url = routes.task(id);
-    const response = await axios.patch(url, { completed });
-    console.log('toggleTaskStateThunk response -', response);
-    return response.data;
   }
 );
 
 const slice = createSlice({
   name: 'tasks',
-  initialState: {
-    tasks: [],
-  },
-  reducers: {
-    initTasks: (state, { payload: { tasks } }) => {
-      state.tasks = [...tasks];
-    },
-    addTask: (state, { payload: { task } }) => {
-      state.tasks.unshift(task);
-    },
-    removeTask: (state, { payload }) => {
-      state.tasks = state.tasks.filter((task) => task.id !== payload.id);
-    },
-    toggleTaskState: (state, { payload: { task } }) => {
-      const currentItem = state.tasks.find(({ id }) => id === task.id);
-      currentItem.completed = task.completed;
-    },
-  },
+  initialState: adapter.getInitialState(),
+  reducers: {},
   extraReducers: {
-    [addTaskThunk.pending]: (state, action) => {
-      console.log('addTaskThunk pending -', action);
-    },
-    [addTaskThunk.fulfilled]: (state, action) => {
-      console.log('addTaskThunk fulfilled', action);
-      const { task } = action.payload;
-      state.tasks.unshift(task);
-    },
-    [addTaskThunk.rejected]: (state, action) => {
-      console.log('addTaskThunk rejected -', action);
-    },
-    [removeTaskThunk.pending]: (state, action) => {
-      console.log('removeTaskThunk pending -', action);
-    },
-    [removeTaskThunk.fulfilled]: (state, action) => {
-      console.log('removeTaskThunk fulfilled -', action);
-    },
-    [removeTaskThunk.rejected]: (state, action) => {
-      console.log('removeTaskThunk rejected -', action);
-    },
-    [toggleTaskStateThunk.pending]: (state, action) => {
-      console.log('toggleTaskStateThunk pending -', action);
-    },
-    [toggleTaskStateThunk.fulfilled]: (state, action) => {
-      console.log('toggleTaskStateThunk fulfilled -', action);
-      const { task } = action.payload;
-      const currentItem = state.tasks.find(({ id }) => id === task.id);
-      currentItem.completed = task.completed;
-    },
-    [toggleTaskStateThunk.rejected]: (state, action) => {
-      console.log('toggleTaskStateThunk rejected -', action);
-    },
+    [fetchAllTasks.fulfilled]: adapter.setAll,
+    [fetchTasksByListId.fulfilled]: adapter.setAll,
+    [addTask.fulfilled]: adapter.addOne,
+    [updateTask.fulfilled]: adapter.upsertOne,
+    [removeTask.fulfilled]: adapter.removeOne,
   },
 });
+
+export const tasksSelectors = adapter.getSelectors((state) => state.tasks);
+export const tasksThunks = {
+  fetchAllTasks,
+  fetchTasksByListId,
+  addTask,
+  updateTask,
+  removeTask,
+};
 
 const actions = { ...slice.actions };
 
