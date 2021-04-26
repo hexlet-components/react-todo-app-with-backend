6afee2d4eb206d462dd3264176300cbb38413478
Vova Afanasiev <grmh9ko@gmail.com>
2021-04-26 22:46:02 +0300

    add yup, add validation on add task

--- src/features/tasks/NewTaskForm.jsx
+++ src/features/tasks/NewTaskForm.jsx
@@ -4,9 +4,20 @@ import React, { useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Formik, Field, Form } from 'formik';
 import axios from 'axios';
+import * as Yup from 'yup';
+import cn from 'classnames';
 
 import { tasksActions } from './tasksSlice';
 import routes from '../../api/routes.js';
+
+const minLength = 3;
+const maxLength = 30;
+const validationSchema = Yup.object().shape({
+  text: Yup.string()
+    .min(minLength, `Too Small! Required length > ${minLength}`)
+    .max(maxLength, `Too Long! Required length < ${maxLength}`)
+    .required('Required!'),
+});
 
 const NewTaskForm = () => {
   const dispatch = useDispatch();
@@ -30,27 +41,40 @@ const NewTaskForm = () => {
   };
 
   return (
-    <Formik initialValues={{ text: '' }} onSubmit={addTask}>
-      {({ isSubmitting }) => (
-        <Form className="form mb-3">
-          <div className="input-group">
-            <Field
-              type="text"
-              className="form-control"
-              placeholder="Please type text..."
-              name="text"
-              readOnly={isSubmitting}
-              innerRef={inputRef}
-            />
-            <button
-              className="btn btn-outline-success"
-              type="submit"
-              disabled={isSubmitting}
-            >
-              Add
-            </button>
-          </div>
-        </Form>
+    <Formik
+      initialValues={{ text: '' }}
+      validationSchema={validationSchema}
+      validateOnBlur={false}
+      onSubmit={addTask}
+    >
+      {({ isSubmitting, isValid, touched, errors }) => (
+        <>
+          <Form className="form mb-3">
+            <div className="input-group">
+              <Field
+                type="text"
+                className={cn('form-control', {
+                  'is-valid': isValid && touched.text,
+                  'is-invalid': !isValid && touched.text,
+                })}
+                placeholder="Please type text..."
+                name="text"
+                readOnly={isSubmitting}
+                innerRef={inputRef}
+              />
+              <button
+                className="btn btn-outline-success"
+                type="submit"
+                disabled={isSubmitting}
+              >
+                Add
+              </button>
+              {errors.text && (
+                <div className="invalid-feedback">{errors.text}</div>
+              )}
+            </div>
+          </Form>
+        </>
       )}
     </Formik>
   );
